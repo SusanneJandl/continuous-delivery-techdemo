8b01955c3af18b14a96619cf9bbf8899f18df503
Susanne Jandl <susanne.jandl@edu.fh-joanneum.at>
2024-12-21 22:01:31 +0100

    update workflow

--- terraform-calculator/main.tf
+++ terraform-calculator/main.tf
@@ -5,21 +5,21 @@ provider "aws" {
 resource "aws_security_group" "instance_sg" {
   name        = "instance_sg"
   description = "Allow SSH and HTTP access"
- 
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -31,11 +31,7 @@ resource "aws_security_group" "instance_sg" {
 resource "aws_instance" "calculator" {
   ami           = "ami-017095afb82994ac7"
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.instance_sg.name] 
-  user_data = <<-EOF
-              #!/bin/bash
-              docker run -d -p 5000:5000 my-calculator
-              EOF
+  security_groups = [aws_security_group.instance_sg.name]
 
   tags = {
     Name = "calculator-instance"
